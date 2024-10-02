bf35788f6c253993486815258b7ea0d51ec1c586
JeffreyZhu <1624410543@qq.com>
2024-10-02 11:49:07 +0800

    对接前后端

--- vuefronted/src/views/manager/Admin.vue
+++ vuefronted/src/views/manager/Admin.vue
@@ -2,11 +2,16 @@
   <div style="width: 100%;height:1000px;padding:40px 40px; background-color: white;border-radius: 4px">
     <el-table :data="tableData" border fit stripe style="width: 100%" :cell-style="{ textAlign: 'center' }"
               :header-cell-style="{ textAlign: 'center' }">
-      <el-table-column prop="date" label="日期"></el-table-column>
-      <el-table-column prop="name" label="姓名"></el-table-column>
-      <el-table-column prop="province" label="省份"></el-table-column>
-      <el-table-column prop="city" label="市区"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+<!--      <el-table-column prop="date" label="日期"></el-table-column>-->
+      <el-table-column prop="id" label="id"></el-table-column>
+      <el-table-column prop="userName" label="姓名"></el-table-column>
+<!--      <el-table-column prop="province" label="省份"></el-table-column>-->
+<!--      <el-table-column prop="city" label="市区"></el-table-column>-->
+<!--      <el-table-column prop="address" label="地址"></el-table-column>-->
+      <el-table-column prop="phone" label="电话"></el-table-column>
+      <el-table-column prop="password" label="密码"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="gender" label="性别"></el-table-column>
       <el-table-column fixed="right" label="操作">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
@@ -20,9 +25,9 @@
           background
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage4"
+          :current-page="currentPage1"
           :page-sizes="[100, 200, 300, 400]"
-          :page-size="100"
+          :page-size="1"
           layout="prev, pager, next"
           :total="1000">
       </el-pagination>
@@ -33,6 +38,7 @@
 </template>
 
 <script>
+import request from "@/utils/request"
 export default {
   methods: {
     handleClick(row) {
@@ -43,46 +49,29 @@ export default {
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
+    },
+    load(){
+      request.get("admin/alldata").then((res) => {
+        if(res.code === '400'){
+          this.tableData = res.data;
+        }
+        else{
+          this.$notify.error(res.msg);
+        }
+      })
     }
 
   },
+  created() {
+    this.load();
+  },
+
 
   data() {
     return {
       input: "",
-      currentPage1: 5,
-      currentPage2: 5,
-      currentPage3: 5,
-      currentPage4: 4,
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1517 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1519 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1516 弄',
-        zip: 200333
-      }]
+      currentPage1: 1,
+      tableData: []
     }
   }
 }
